a7719c6ac800b7c659f205c1f7744913e3b79c74
enzoocmagalhaes-lab <enzoocmagalhaes@gmail.com>
2026-07-06 20:40:19 -0300

    prog25.py

---
+++ prog25.py
@@ -0,0 +1,7 @@
+v1 = int(input('Digite o seu primeiro valor: '))
+v2 = int(input('Digite o seu segundo valor: '))
+
+if v1 > v2:
+    print('O seu primeiro valor é maior')
+else:
+    print('O seu segundo valor é maior')
