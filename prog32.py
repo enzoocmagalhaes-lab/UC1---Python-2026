7ab79a0a7a64f46aafa3f11e7ca74c639f5a3234
enzoocmagalhaes-lab <enzoocmagalhaes@gmail.com>
2026-07-08 20:29:12 -0300

    commit

---
+++ prog32.py
@@ -0,0 +1,5 @@
+m = 0
+for i in range(1,6):
+    x = int(input('Digite o valor: '))
+    m = m + x
+print(f'O valor de m é {m}')
---
+++ prog33.py
@@ -0,0 +1,4 @@
+valor = int(input('Digite o seu valor: '))
+for i in range(1,11):
+    v = valor * i
+    print(f'Tabuada de {valor}: {valor} * {i} = {v}')
